afca8cf13d7a3268d9c39a9a8dc321669b319f34
Zak Laughton <zlaughton@gmail.com>
2019-12-11 09:07:09 -0600

    finish day 4

---
+++ 2019/day-4/p1-code.js
@@ -0,0 +1,23 @@
+const input = [153517, 630395];
+const inputMin = input[0];
+const inputMax = input[1];
+
+const containsDoubles = password => {
+  const passwordArray = password.toString().split('');
+  return passwordArray.some((digit, index, array) => array[index + 1] === digit);
+};
+const areAllNumbersIncreasing = password => {
+  const passwordArray = password.toString().split('');
+  return passwordArray.every(
+    (digit, index, array) => digit <= array[index + 1] || index + 1 === array.length
+  );
+};
+
+let count = 0;
+
+for (let i = inputMin; i <= inputMax; i++) {
+  if (containsDoubles(i) && areAllNumbersIncreasing(i)) {
+    count += 1;
+  }
+}
+console.log(count);
