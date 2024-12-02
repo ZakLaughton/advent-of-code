9933225d94e0006b66a02a4769f47504b5230008
Zak Laughton <me@zaklaughton.dev>
2024-12-02 16:40:37 -0600

    finished day 1 solutions

--- 2024/day-01/p1-code.ts
+++ 2024/day-01/p1-code.ts
@@ -1,10 +1,77 @@
-const fs = require('fs');
+// @ts-nocheck
 
-// @ts-expect-error
-fs.readFile('p1-input.txt', (err, data) => {
-  if (err) throw err;
+import { getTextInputFromFile, test } from '../../utils';
 
-  console.log(data.toString());
-});
+const leftNumbers = [];
+const rightNumbers = [];
 
-// test(add, [1, 2], 3);
+const input = getTextInputFromFile('p1-input.txt');
+const lines = splitTextInputIntoLines(input);
+for (const line of lines) {
+  const [left, right] = splitIntoLeftAndRightNumber(line);
+  leftNumbers.push(left);
+  rightNumbers.push(right);
+}
+leftNumbers.sort((a, b) => a - b);
+rightNumbers.sort((a, b) => a - b);
+console.log(
+  'Answer>>>',
+  getTotalDistanceBetweenLists(leftNumbers, rightNumbers)
+);
+
+/******
+ * Functions
+ */
+
+function splitIntoLeftAndRightNumber(lineText: string): [number, number] {
+  const [left, right] = lineText.split(' ').filter((x) => x !== '');
+  return [Number(left), Number(right)];
+}
+
+function splitTextInputIntoLines(input: string): string[] {
+  return input.split('\n');
+}
+
+function getTotalDistanceBetweenLists(left: number[], right: number[]): number {
+  let totalDistance = 0;
+  for (let i = 0; i < left.length; i++) {
+    totalDistance += Math.abs(left[i] - right[i]);
+  }
+  return totalDistance;
+}
+
+/*********
+ * TESTS *
+ *********/
+
+test(splitIntoLeftAndRightNumber, ['25095   46875'], [25095, 46875]);
+
+const splitTextInputIntoLinesInput = `69214   60950
+83241   49638
+37930   31308
+50722   94914
+93164   82798
+80918   72850
+17490   79421`;
+
+test(
+  splitTextInputIntoLines,
+  [splitTextInputIntoLinesInput],
+  [
+    '69214   60950',
+    '83241   49638',
+    '37930   31308',
+    '50722   94914',
+    '93164   82798',
+    '80918   72850',
+    '17490   79421',
+  ]
+);
+test(
+  getTotalDistanceBetweenLists,
+  [
+    [1, 2, 3, 3, 3, 4],
+    [3, 3, 3, 4, 5, 9],
+  ],
+  11
+);
---
+++ 2024/day-01/p2-code.ts
@@ -0,0 +1,108 @@
+// @ts-nocheck
+
+import { get } from 'lodash';
+import { getTextInputFromFile, test } from '../../utils';
+
+const leftNumbers = [];
+const rightNumbers = [];
+
+const input = getTextInputFromFile('p1-input.txt');
+const lines = splitTextInputIntoLines(input);
+for (const line of lines) {
+  const [left, right] = splitIntoLeftAndRightNumber(line);
+  leftNumbers.push(left);
+  rightNumbers.push(right);
+}
+
+const leftNumberOccurrencesInRight = getNumberOccurrencesInList1InList2(
+  leftNumbers,
+  rightNumbers
+);
+let similarityScore = 0;
+for (const number of leftNumbers) {
+  similarityScore += number * leftNumberOccurrencesInRight[number];
+}
+
+console.log('Answer>>>', similarityScore);
+
+/******
+ * Functions
+ */
+
+function splitIntoLeftAndRightNumber(lineText: string): [number, number] {
+  const [left, right] = lineText.split(' ').filter((x) => x !== '');
+  return [Number(left), Number(right)];
+}
+
+function splitTextInputIntoLines(input: string): string[] {
+  return input.split('\n');
+}
+
+function getSimilarityScoreBetweenLists(
+  left: number[],
+  right: number[]
+): number {
+  // Count number of times items in list 1 appears in list 2
+}
+
+function countNumberOccurrencesInList(number: number, list: number[]): number {
+  let count = 0;
+  for (const item of list) {
+    if (item === number) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// Gets the total number of times a number in list 1 appears in list 2, returns a dictionary with that number as the key and the count as the value
+function getNumberOccurrencesInList1InList2(
+  list1: number[],
+  list2: number[]
+): { [key: number]: number } {
+  const occurrences = {};
+  for (const number of list1) {
+    if (occurrences[number] === undefined) {
+      occurrences[number] = countNumberOccurrencesInList(number, list2);
+    }
+  }
+  return occurrences;
+}
+
+/*********
+ * TESTS *
+ *********/
+
+test(splitIntoLeftAndRightNumber, ['25095   46875'], [25095, 46875]);
+
+const splitTextInputIntoLinesInput = `69214   60950
+83241   49638
+37930   31308
+50722   94914
+93164   82798
+80918   72850
+17490   79421`;
+
+test(
+  splitTextInputIntoLines,
+  [splitTextInputIntoLinesInput],
+  [
+    '69214   60950',
+    '83241   49638',
+    '37930   31308',
+    '50722   94914',
+    '93164   82798',
+    '80918   72850',
+    '17490   79421',
+  ]
+);
+
+test(countNumberOccurrencesInList, [3, [1, 2, 3, 3, 3, 4]], 3);
+test(
+  getNumberOccurrencesInList1InList2,
+  [
+    [1, 2, 3, 3, 3, 4],
+    [3, 3, 3, 4, 5, 9],
+  ],
+  { 1: 0, 2: 0, 3: 3, 4: 1 }
+);
