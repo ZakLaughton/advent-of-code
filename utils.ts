ad67070a48a101d35d46337ae7abd71d2bdeb00f
Zak Laughton <me@zaklaughton.dev>
2024-12-04 14:12:25 -0600

    update test to handle errors

--- utils.ts
+++ utils.ts
@@ -3,28 +3,36 @@ export function test<T>(
   input: any[],
   expected: T
 ) {
-  const result = func(...input); // Spread input in case it's an array of arguments
-  if (result instanceof Set && expected instanceof Set) {
-    const resultArray = Array.from(result).sort();
-    const expectedArray = Array.from(expected).sort();
-    if (JSON.stringify(resultArray) === JSON.stringify(expectedArray)) {
+  try {
+    const result = func(...input); // Spread input in case it's an array of arguments
+    if (result instanceof Set && expected instanceof Set) {
+      const resultArray = Array.from(result).sort();
+      const expectedArray = Array.from(expected).sort();
+      if (JSON.stringify(resultArray) === JSON.stringify(expectedArray)) {
+        console.log(`✅ Test passed. Input: ${JSON.stringify(input)}`);
+      } else {
+        console.error(
+          `❌ Test failed. Input: ${JSON.stringify(
+            input
+          )}, Expected: ${JSON.stringify(expectedArray)}, Got: ${JSON.stringify(
+            resultArray
+          )}`
+        );
+      }
+    } else if (JSON.stringify(result) === JSON.stringify(expected)) {
       console.log(`✅ Test passed. Input: ${JSON.stringify(input)}`);
     } else {
       console.error(
         `❌ Test failed. Input: ${JSON.stringify(
           input
-        )}, Expected: ${JSON.stringify(expectedArray)}, Got: ${JSON.stringify(
-          resultArray
+        )}, Expected: ${JSON.stringify(expected)}, Got: ${JSON.stringify(
+          result
         )}`
       );
     }
-  } else if (JSON.stringify(result) === JSON.stringify(expected)) {
-    console.log(`✅ Test passed. Input: ${JSON.stringify(input)}`);
-  } else {
+  } catch (e) {
     console.error(
-      `❌ Test failed. Input: ${JSON.stringify(
-        input
-      )}, Expected: ${JSON.stringify(expected)}, Got: ${JSON.stringify(result)}`
+      `❌ Test failed. Input: ${JSON.stringify(input)}, Error: ${e}`
     );
   }
 }
