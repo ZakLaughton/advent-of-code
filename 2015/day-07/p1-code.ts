563a4f3c7a650b1d7da903327af09ac69a35b0cb
Zak Laughton <me@zaklaughton.dev>
2024-12-04 06:16:41 -0600

    start 2015 day 7

--- 2015/day-07/p1-code.ts
+++ 2015/day-07/p1-code.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { test } from '../../utils';
 
 type Operation = 'ASSIGN' | 'AND' | 'OR' | 'NOT' | 'LSHIFT' | 'RSHIFT';
@@ -6,38 +7,97 @@ type ParsedInstruction = {
   input: (string | number)[];
   assignee: string;
 };
+type ParsedOperation = {
+  operation: Operation;
+  input: (string | number)[];
+};
 
 /** FUNCTIONS */
-function parseInstruction(instruction: string): ParsedInstruction {}
+function parseInstruction(instruction: string): ParsedInstruction {
+  console.log('Parsing instruction:', instruction);
+  const regex = /^(.*?) -> (\w+)/;
+  const match = instruction.match(regex);
+
+  const [, operation, assignee] = match;
+  console.log('match>>>', match);
+}
+
+// parses the operation part of an instruction
+function parseOperation(input: string): ParsedOperation {
+  console.log('Parsing operation:', input);
+  let operation = '';
+  let input = [];
+  // test for NOT
+  if (input.startsWith('NOT')) {
+    operation = 'NOT';
+    const regex = /(\w+)$/;
+    const match = input.match(regex);
+
+    console.log('match>>>', match);
+  }
+}
 
 /** TESTS */
-test(parseInstruction, ['NOT dq -> dr'], {
+console.log('\n\n🧪 Testing parseInstruction');
+// test(parseInstruction, ['NOT dq -> dr'], {
+//   operation: 'NOT',
+//   input: ['dq'],
+//   assignee: 'dr',
+// });
+// test(parseInstruction, ['kg OR kf -> kh'], {
+//   operation: 'OR',
+//   input: ['kg', 'kf'],
+//   assignee: 'kh',
+// });
+// test(parseInstruction, ['44430 -> b'], {
+//   operation: 'ASSIGN',
+//   input: [44430],
+//   assignee: 'b',
+// });
+// test(parseInstruction, ['y AND ae -> ag'], {
+//   operation: 'AND',
+//   input: ['y', 'ae'],
+//   assignee: 'ag',
+// });
+// test(parseInstruction, ['kk RSHIFT 3 -> km'], {
+//   operation: 'RSHIFT',
+//   input: ['kk', 3],
+//   assignee: 'km',
+// });
+// test(parseInstruction, ['bk LSHIFT 1 -> ce'], {
+//   operation: 'LSHIFT',
+//   input: ['bk', 1],
+//   assignee: 'ce',
+// });
+
+console.log('\n\n🧪 Testing parseOperation');
+test(parseOperation, ['NOT dq'], {
   operation: 'NOT',
   input: ['dq'],
   assignee: 'dr',
 });
-test(parseInstruction, ['kg OR kf -> kh'], {
-  operation: 'OR',
-  input: ['kg', 'kf'],
-  assignee: 'kh',
-});
-test(parseInstruction, ['44430 -> b'], {
-  operation: 'ASSIGN',
-  input: [44430],
-  assignee: 'b',
-});
-test(parseInstruction, ['y AND ae -> ag'], {
-  operation: 'AND',
-  input: ['y', 'ae'],
-  assignee: 'ag',
-});
-test(parseInstruction, ['kk RSHIFT 3 -> km'], {
-  operation: 'RSHIFT',
-  input: ['kk', 3],
-  assignee: 'km',
-});
-test(parseInstruction, ['bk LSHIFT 1 -> ce'], {
-  operation: 'LSHIFT',
-  input: ['bk', 1],
-  assignee: 'ce',
-});
+// test(parseOperation, ['kg OR kf'], {
+//   operation: 'OR',
+//   input: ['kg', 'kf'],
+//   assignee: 'kh',
+// });
+// test(parseOperation, ['44430'], {
+//   operation: 'ASSIGN',
+//   input: [44430],
+//   assignee: 'b',
+// });
+// test(parseOperation, ['y AND ae'], {
+//   operation: 'AND',
+//   input: ['y', 'ae'],
+//   assignee: 'ag',
+// });
+// test(parseOperation, ['kk RSHIFT 3'], {
+//   operation: 'RSHIFT',
+//   input: ['kk', 3],
+//   assignee: 'km',
+// });
+// test(parseOperation, ['bk LSHIFT 1'], {
+//   operation: 'LSHIFT',
+//   input: ['bk', 1],
+//   assignee: 'ce',
+// });
