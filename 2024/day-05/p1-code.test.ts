3362c60c78d7f86ec3f38ea67e6ea7645be5b57f
Zak Laughton <me@zaklaughton.dev>
2024-12-05 11:08:49 -0600

    finished part 1!

---
+++ 2024/day-05/p1-code.test.ts
@@ -0,0 +1,66 @@
+import { test } from '../../utils';
+import {
+  getMiddlePageNumber,
+  getMiddlePageTotal,
+  getSolution,
+  isPageValid,
+  isUpdateValid,
+  parseRulesAndUpdatesFromInput,
+} from './p1-code';
+
+/** TESTS */
+const testInput = `47|53
+97|13
+97|61
+13|97
+
+75,47,61,53,29
+97,61,53,29,13`;
+
+console.log('\n\n🧪 Testing getSolution');
+test(getSolution, [testInput], 61);
+
+console.log('\n\n🧪 Testing parseRulesAndUpdatesFromInput');
+const parsedRulesOutput = {
+  rules: [
+    [47, 53],
+    [97, 13],
+    [97, 61],
+    [13, 97],
+  ],
+  updates: [
+    [75, 47, 61, 53, 29],
+    [97, 61, 53, 29, 13],
+  ],
+};
+test(parseRulesAndUpdatesFromInput, [testInput], parsedRulesOutput);
+
+console.log('\n\n🧪 Testing getMiddlePageTotal');
+test(getMiddlePageTotal, [parsedRulesOutput], 61);
+
+console.log('\n\n🧪 Testing isUpdateValid');
+test(isUpdateValid, [parsedRulesOutput.rules, [75, 47, 61, 53, 29]], true);
+test(isUpdateValid, [parsedRulesOutput.rules, [97, 61, 53, 29, 13]], false);
+
+console.log('\n\n🧪 Testing isPageValid');
+test(
+  isPageValid,
+  [{ rules: parsedRulesOutput.rules, startPage: 47, pagesAfter: [61, 53, 29] }],
+  true
+);
+test(
+  isPageValid,
+  [
+    {
+      rules: parsedRulesOutput.rules,
+      startPage: 97,
+      pagesAfter: [61, 53, 29, 13],
+    },
+  ],
+  false
+);
+
+console.log('\n\n🧪 Testing getMiddlePageNumber');
+test(getMiddlePageNumber, [[75, 47, 61, 53, 29]], 61);
+test(getMiddlePageNumber, [[97, 61, 53, 29, 13]], 53);
+test(getMiddlePageNumber, [[75, 29, 13]], 29);
