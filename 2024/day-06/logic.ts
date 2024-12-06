7c384221374dcd28d8f23ccc1651cbfa90231e07
Zak Laughton <me@zaklaughton.dev>
2024-12-06 11:03:58 -0600

    got initial patroll started working

--- 2024/day-06/logic.ts
+++ 2024/day-06/logic.ts
@@ -1,11 +1,24 @@
+type Direction = 'up' | 'right' | 'left' | 'down';
+type LocationType = '.' | '^' | '#' | '';
+type Coordinates = [number, number];
+type Grid = string[];
+
 // Counts number of spaces a guard visits before leaving a grid
 // The guard starts at ^, and stops before every # and turns right.
 // See README in this directory for more detail
-export function countVisitedSpaces(input: string): number {
+export function countVisitedSpaces(grid: Grid): number {
   const visitedSpaces = new Set();
-  let guardLocation = getGuardLocation(input);
+  let guardLocation = getGuardLocation(grid);
+  let guardDirection: Direction = 'up';
 
   while (isInGrid(grid, guardLocation)) {
+    const startingLocation = guardLocation;
+    const startingDirection = guardDirection;
+    const { endingLocation, endingDirection, visitedSpaces } = patrol({
+      grid,
+      startingLocation,
+      startingDirection,
+    });
     //   move guard until obstacle or out of bounds
     //   rotate guard
     //   update spaces visited
@@ -15,7 +28,7 @@ export function countVisitedSpaces(input: string): number {
 
 // Returns the starting location of the guard as
 // [rowIndex, columnIndex]
-export function getGuardLocation(grid: string[]): [number, number] {
+export function getGuardLocation(grid: Grid): Coordinates {
   for (const [index, row] of grid.entries()) {
     const column = row.indexOf('^');
     if (column >= 0) {
@@ -27,15 +40,83 @@ export function getGuardLocation(grid: string[]): [number, number] {
 }
 
 // Determines if a set of coordinates is in-bounds for a given grid
-export function isInGrid(
-  grid: string[],
-  guardLocation: [number, number]
-): boolean {
+export function isInGrid(grid: Grid, coordinates: Coordinates): boolean {
   const rowCount = grid.length;
   const columnCount = grid[0].length;
 
   const maxRowIndex = rowCount - 1;
   const maxColumnIndex = columnCount - 1;
 
-  return guardLocation[0] <= maxRowIndex && guardLocation[1] <= maxColumnIndex;
+  return coordinates[0] <= maxRowIndex && coordinates[1] <= maxColumnIndex;
+}
+
+interface PatrolInput {
+  grid: Grid;
+  startingLocation: Coordinates;
+  startingDirection: Direction;
+}
+// Moves the guard to an obstacle or until it's out of bounds.
+// Returns details about the position, direction, and spaces traveled
+export function patrol({
+  grid,
+  startingLocation,
+  startingDirection,
+}: PatrolInput): {
+  endingLocation: Coordinates;
+  endingDirection: Direction;
+} {
+  const visitedSpaces: Coordinates[] = [];
+  let currentLocation = startingLocation;
+  let currentDirection = startingDirection;
+
+  const {
+    coordinates: nextLocationCoordinates,
+    locationType: nextLocationType,
+  } = getNextLocation({ grid, currentLocation, currentDirection });
+
+  while (isInGrid(grid, currentLocation) && nextLocationType !== '#')
+    // while next space is not an obstacle && current space is in the grid
+    // move in direction
+
+    return {
+      endingDirection: currentDirection,
+      endingLocation: currentLocation,
+    };
+}
+
+interface GetNextLocationInput {
+  grid: Grid;
+  currentLocation: Coordinates;
+  currentDirection: Direction;
+}
+
+export function getNextLocation({
+  grid,
+  currentLocation,
+  currentDirection,
+}: GetNextLocationInput): {
+  coordinates: Coordinates;
+  locationType: '.' | '^' | '#' | '';
+} {
+  // Amount to change coordinates for moving in any direction
+  const directionDelta: Record<Direction, Coordinates> = {
+    up: [-1, 0],
+    right: [0, 1],
+    left: [0, -1],
+    down: [1, 0],
+  };
+
+  const nextRowIndex = (currentLocation[0] +=
+    directionDelta[currentDirection][0]);
+  const nextColumnIndex = (currentLocation[1] +=
+    directionDelta[currentDirection][1]);
+  const nextCoordinates: Coordinates = [nextRowIndex, nextColumnIndex];
+  const locationType = (isInGrid(grid, nextCoordinates)
+    ? grid[nextRowIndex][nextColumnIndex]
+    : '') as LocationType;
+
+  return {
+    coordinates: [nextRowIndex, nextColumnIndex],
+    locationType,
+  };
 }
--- 2024/day-06/tests.ts
+++ 2024/day-06/tests.ts
@@ -1,5 +1,5 @@
 import { test } from '../../utils';
-import { getGuardLocation, isInGrid } from './logic';
+import { getGuardLocation, getNextLocation, isInGrid, patrol } from './logic';
 import { getPart1Solution } from './solutions';
 
 // Copied from https://adventofcode.com/2024/day/6
@@ -20,7 +20,7 @@ console.log(
   '\n\n***********************\n**** PART 1 TESTING ***\n***********************'
 );
 console.log('\n\n🧪 Testing getPart1Solution');
-test(getPart1Solution, [exampleGrid], 41);
+// test(getPart1Solution, [exampleGrid], 41);
 
 console.log('\n\n🧪 Testing getGuardLocation');
 test(getGuardLocation, [exampleGrid], [6, 4]);
@@ -30,6 +30,79 @@ test(isInGrid, [exampleGrid, [6, 4]], true);
 test(isInGrid, [exampleGrid, [10, 4]], false);
 test(isInGrid, [exampleGrid, [6, 10]], false);
 test(isInGrid, [exampleGrid, [10, 10]], false);
+
+console.log('\n\n🧪 Testing patrol');
+// Test 4 directions
+test(
+  patrol,
+  [{ grid: exampleGrid, startingLocation: [6, 4], startingDirection: 'up' }],
+  {
+    endingLocation: [1, 4],
+    endingDirection: 'right',
+  }
+);
+test(
+  patrol,
+  [{ grid: exampleGrid, startingLocation: [1, 4], startingDirection: 'right' }],
+  {
+    endingLocation: [1, 8],
+    endingDirection: 'down',
+  }
+);
+test(
+  patrol,
+  [{ grid: exampleGrid, startingLocation: [1, 8], startingDirection: 'down' }],
+  {
+    endingLocation: [6, 8],
+    endingDirection: 'left',
+  }
+);
+test(
+  patrol,
+  [{ grid: exampleGrid, startingLocation: [6, 8], startingDirection: 'left' }],
+  {
+    endingLocation: [6, 2],
+    endingDirection: 'up',
+  }
+);
+// Test moving out of bounds
+test(
+  patrol,
+  [{ grid: exampleGrid, startingLocation: [7, 7], startingDirection: 'down' }],
+  {
+    endingLocation: [10, 7],
+    endingDirection: 'up',
+  }
+);
+
+console.log('\n\n🧪 Testing getNextLocation');
+// Test '.'
+test(
+  getNextLocation,
+  [{ grid: exampleGrid, currentLocation: [6, 4], currentDirection: 'up' }],
+  {
+    coordinates: [5, 4],
+    locationType: '.',
+  }
+);
+// Test '#'
+test(
+  getNextLocation,
+  [{ grid: exampleGrid, currentLocation: [1, 4], currentDirection: 'up' }],
+  {
+    coordinates: [0, 4],
+    locationType: '#',
+  }
+);
+// Test out of bounds
+test(
+  getNextLocation,
+  [{ grid: exampleGrid, currentLocation: [9, 7], currentDirection: 'down' }],
+  {
+    coordinates: [10, 7],
+    locationType: '',
+  }
+);
 
 console.log(
   '\n\n***********************\n**** PART 2 TESTING ***\n***********************'
