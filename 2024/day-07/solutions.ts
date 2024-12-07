dbe8f723c31dd9ab2af120db0ec78f407e964ef1
Zak Laughton <me@zaklaughton.dev>
2024-12-06 22:27:08 -0600

    set up empty files for 2024 day 7

---
+++ 2024/day-07/solutions.ts
@@ -0,0 +1,10 @@
+// import { getTextInputFromFile } from '../../utils';
+
+// const input = getTextInputFromFile('./input.txt');
+
+// console.log('Part 1 Solution>>>', getPart1Solution(input));
+// console.log('Part 2 Solution>>>', getPart2Solution(input));
+
+// export function getPart1Solution(input: string): number {}
+
+// export function getPart2Solution(input: string): number {}
