8b50a9279866629f449cb678c014251f373ffbd1
Zak Laughton <me@zaklaughton.dev>
2024-12-09 19:13:10 -0600

    set up test for count antinodes

--- 2024/day-08/tests.ts
+++ 2024/day-08/tests.ts
@@ -3,9 +3,13 @@ import {
   logFunctionTestingHeader,
   test,
 } from '../../utils';
+import { countAntinodes } from './logic';
 import { getPart1Solution } from './solutions';
 
 const exampleInput = getTextInputFromFileInLines('./example-input.txt');
 
 logFunctionTestingHeader('getPart1Solution()');
 test(getPart1Solution, [exampleInput], 14);
+
+logFunctionTestingHeader('countAntinodes');
+test(countAntinodes, [exampleInput], 14);
