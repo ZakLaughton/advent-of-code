adcca805a0aabbe7baa881f575937bbfd58f276b
Zak Laughton <me@zaklaughton.dev>
2024-12-10 09:08:02 -0600

    finished part 1

--- 2024/day-09/solutions.ts
+++ 2024/day-09/solutions.ts
@@ -1,16 +1,15 @@
 import { getTextInputFromFile } from '../../utils';
-import { fragmentDisk, getAllFileBlocks } from './logic';
+import { fragmentDisk, getAllFileBlocks, getChecksum } from './logic';
 
 const input = getTextInputFromFile('./input.txt');
-// console.log('PART 1 ANSWER>>>', getPart1Solution(input));
+console.log('PART 1 ANSWER>>>', getPart1Solution(input));
 // console.log('PART 2 ANSWER>>>', getPart2Solution(input));
 
 export function getPart1Solution(input: string): number {
   const fileBlocks = getAllFileBlocks(input);
   const fragmentedDiskMap = fragmentDisk(input, fileBlocks);
-  const checksum = getChecksum(fragmentedDiskMap);
-  // Use full string to alternating fill in existing files and other files from the back backwards
-  // calculate total
+  const checkSum = getChecksum(fragmentedDiskMap);
+  return checkSum;
 }
 
 // export function getPart2Solution(input: string[]): number {
