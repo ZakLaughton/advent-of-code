f32e2c3de06f9122887738c46a3d242afca30c2c
Zak Laughton <me@zaklaughton.dev>
2024-12-09 19:15:00 -0600

    set up skeleton for getAntennaLocations

--- 2024/day-08/logic.ts
+++ 2024/day-08/logic.ts
@@ -3,8 +3,11 @@
 type AntennaLocations = Record<string, [number, number][]>;
 
 export function countAntinodes(input: string[]): number {
+  const antennaLocations = getAntennaLocations(input);
   // create a dictionary of locations of all antennas, key is frequency, value is a array of locations
   // for each frequency, get antinode locations from their locations
   // filter out results not in the grid
   // return total count of antinodes
 }
+
+export function getAntennaLocations(grid: string): AntennaLocations {}
