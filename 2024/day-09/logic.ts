e09ebf078a83083ab52ec828300c600450b175fc
Zak Laughton <me@zaklaughton.dev>
2024-12-10 09:16:29 -0600

    cleaned up some logic

--- 2024/day-09/logic.ts
+++ 2024/day-09/logic.ts
@@ -1,17 +1,13 @@
 // Returns the full string of file blocks with no gaps present
 export function getAllFileBlocks(diskMap: string): number[] {
-  const fileBlocks = [];
-  let fileNumber = 0;
-  for (let i = 0; i < diskMap.length; i++) {
-    // Skip odd indices, which represent empty space
-    if (i % 2 === 1) {
-      continue;
-    }
-    for (let j = 0; j < Number(diskMap[i]); j++) {
-      fileBlocks.push(fileNumber);
-    }
-    fileNumber++;
+  const fileBlocks: number[] = [];
+  let fileID = 0;
+
+  for (let i = 0; i < diskMap.length; i += 2) {
+    const length = Number(diskMap[i]);
+    fileBlocks.push(...new Array(length).fill(fileID++));
   }
+
   return fileBlocks;
 }
 
