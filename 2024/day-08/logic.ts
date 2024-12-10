c2829c8a2998de801ad34c6a36d24ffe066dd641
Zak Laughton <me@zaklaughton.dev>
2024-12-09 20:18:47 -0600

    got skeleton down to bottom with first tests passing

--- 2024/day-08/logic.ts
+++ 2024/day-08/logic.ts
@@ -4,7 +4,7 @@ type AntennaLocations = Record<string, [number, number][]>;
 
 export function countAntinodes(input: string[]): number {
   const antennaLocations = getAntennaLocations(input);
-  // create a dictionary of locations of all antennas, key is frequency, value is a array of locations
+  const antinodeLocations = getAntinodeLocations(antennaLocations);
   // for each frequency, get antinode locations from their locations
   // filter out results not in the grid
   // return total count of antinodes
@@ -26,4 +26,55 @@ export function getAntennaLocations(grid: string[]): AntennaLocations {
     }
   }
   return antennaLocations;
+}
+
+export function getAntinodeLocations(
+  antennaLocations: AntennaLocations
+): [number, number][] {
+  const antinodeLocations: [number, number][] = [];
+  for (const frequency of Object.keys(antennaLocations)) {
+    antinodeLocations.concat(
+      getAntinodeLocationsForGivenLocations(antennaLocations[frequency])
+    );
+    // recursively get antinode locations for each antenna
+  }
+  return antinodeLocations.sort(sortLocationArray);
+}
+
+// Takes all locations for a given frequency, returns all possible antinode locations
+export function getAntinodeLocationsForGivenLocations(
+  locations: [number, number][]
+): [number, number][] {
+  const antinodeLocations: [number, number][] = [];
+  for (const location of locations) {
+    const locationsToTestAgainst = locations.slice(1);
+    for (const testingLocation of locationsToTestAgainst) {
+      antinodeLocations.push(
+        getAntinodeLocationsForTwoAntennas(location, testingLocation)
+      );
+    }
+  }
+  return antinodeLocations.sort(sortLocationArray);
+}
+
+export function getAntinodeLocationsForTwoAntennas(
+  antennaA: [number, number],
+  antennaB: [number, number]
+): [number, number][] {
+  const antinodeARow = antennaA[0] + (antennaA[0] - antennaB[0]);
+  const antinodeAColumn = antennaA[1] + (antennaA[1] - antennaB[1]);
+  const antinodeBRow = antennaB[0] + (antennaB[0] - antennaA[0]);
+  const antinodeBColumn = antennaB[1] + (antennaB[1] - antennaA[1]);
+
+  const antinodeLocations: [number, number][] = [
+    [antinodeARow, antinodeAColumn],
+    [antinodeBRow, antinodeBColumn],
+  ];
+  return antinodeLocations.sort(sortLocationArray);
+}
+
+// Used with .sort to sort a location array by rowIndex, then char index
+function sortLocationArray(a: [number, number], b: [number, number]): number {
+  if (a[0] !== b[0]) return a[0] - b[0];
+  return (a[1] = b[1]);
 }
--- 2024/day-08/tests.ts
+++ 2024/day-08/tests.ts
@@ -3,7 +3,13 @@ import {
   logFunctionTestingHeader,
   test,
 } from '../../utils';
-import { countAntinodes, getAntennaLocations } from './logic';
+import {
+  countAntinodes,
+  getAntennaLocations,
+  getAntinodeLocations,
+  getAntinodeLocationsForGivenLocations,
+  getAntinodeLocationsForTwoAntennas,
+} from './logic';
 import { getPart1Solution } from './solutions';
 
 const exampleInput = getTextInputFromFileInLines('./example-input.txt');
@@ -28,3 +34,79 @@ test(getAntennaLocations, [exampleInput], {
     [9, 9],
   ],
 });
+
+logFunctionTestingHeader('getAntinodeLocations()');
+test(
+  getAntinodeLocations,
+  [
+    {
+      '0': [
+        [1, 8],
+        [2, 5],
+        [3, 7],
+        [4, 4],
+      ],
+      A: [
+        [5, 6],
+        [8, 8],
+        [9, 9],
+      ],
+    },
+  ],
+  [
+    [0, 6],
+    [0, 11],
+    [1, 3],
+    [2, 4],
+    [2, 10],
+    [3, 2],
+    [4, 9],
+    [5, 1],
+    [5, 6],
+    [6, 3],
+    [7, 0],
+    [7, 7],
+    [10, 10],
+    [11, 10],
+  ]
+);
+
+/**
+ * Sample grid to test:
+ *
+ * ..A..
+ * .A.A.
+ * .....
+ */
+logFunctionTestingHeader('getAntinodeLocationsForGivenLocations()');
+test(
+  getAntinodeLocationsForGivenLocations,
+  [
+    [
+      [0, 2],
+      [1, 1],
+      [1, 3],
+    ],
+  ],
+  [
+    [-1, 1],
+    [-1, 3],
+    [1, -1],
+    [1, 5],
+    [2, 0],
+    [2, 4],
+  ]
+);
+
+logFunctionTestingHeader('getAntinodeLocationsForTwoAntennas()');
+test(
+  getAntinodeLocationsForTwoAntennas,
+  [
+    [0, 2],
+    [1, 1],
+  ],
+  [
+    [-1, 3],
+    [2, 0],
+  ]
+);
